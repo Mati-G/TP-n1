af2f9c877660cf5bab7e4a2e3c342ed3406af9b7
Mati-G <Matiasn.gimenez@hotmail.com>
2021-05-12 11:35:12 -0300

    Entrega del primer TP Calculadora

---
+++ ASDASD/sumare.js
@@ -0,0 +1,17 @@
+let sumar = function(a,b) {
+    return a + b
+}
+
+let restar = function(a,b) {
+    return a - b
+}
+
+let multiplicar = function(a,b) {
+    return a * b
+}
+
+let dividir = function(a,b) {
+    return a / b
+}
+
+console.log (multiplicar (15,15))
---
+++ calculadora1.js
@@ -0,0 +1,9 @@
+let sumar = require("./sumar");
+
+let restar = require("./restar");
+
+let multiplicar = require("./multiplicar");
+
+let dividir = require("./dividir");
+
+console.log(dividir(0,10));
